33b83815bd7dca419a7d1e60e45046d32edbf314
SeamusLynchTUD <c23471656@mytudublin.ie>
2024-02-22 15:54:02 +0000

    Tried adding bullet code. Pressing space crashes the game

--- Game Design Examples/MovementScript.gd
+++ Game Design Examples/MovementScript.gd
@@ -1,9 +1,20 @@
 extends CharacterBody2D
 
+@export var bullet:PackedScene
+@export var bullet_spawn:Node2D
+
+var can_fire = true;
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	bullet = load("res://bullet.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	position.x = get_global_mouse_position().x
+	if Input.is_action_pressed("fire") and can_fire:
+		var b = bullet.instantiate()
+		b.global_position = bullet_spawn.global_position
+		b.global_rotation = bullet_spawn.global_rotation
+		get_tree().get_root().add_child(b) 
+		can_fire = false
+		$Timer.start()
